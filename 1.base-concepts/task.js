78d43dbc11f84a6306b5b9db7ee15f1f4c5981c6
Olga <o.n.p@mail.ru>
2023-09-20 14:16:25 +0300

    Add HomeWork 1.base-concepts task 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,16 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let d = b**2-4*a*c;
+
+  if (d === 0) {
+    arr[0] = -b/(2*a)
+  } 
+  
+  if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a)
+    arr[1] = (-b - Math.sqrt(d) )/(2*a)
+  }
   
   return arr;
 }
