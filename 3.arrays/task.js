2826234fadd4d178dc885057c82bb86a4c5f0e5e
Olga <o.n.p@mail.ru>
2023-09-25 22:01:29 +0300

    Add homeWork 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
 function compareArrays(arr1, arr2) {
-  
+    
+    if (arr1.every((value, index) => value === arr2[index]) === true && arr2.every((value, index) => value === arr1[index]) === true) {
+        return true;
+    } else {
+        return false
+    }
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+    let result = users.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, result) => {
+        acc += item;
+        if (index === result.length - 1) {
+            return acc / result.length;
+        }
+        return acc;
+    }, 0);
+
+    return result;
 }
