1ffd26fe3e66c684b7428b4612ddf58607668ab4
Olga <o.n.p@mail.ru>
2023-10-08 16:09:20 +0300

    Add homeWork 5.classes task 1,2 (all)

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -100,6 +100,33 @@ class Library {
             this.books.splice(index, 1);
             return result;
         }
-    }
-        
+    }        
 }
+
+// Проверка корректности работы классов и методов
+
+const library = new Library("Библиотека имени Ленина");
+
+library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание рассказов",2019,1008));
+library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине",1972,168));
+library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1919, 138));
+library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+console.log(library.findBookBy("releaseDate", 1919).name); //Машина времени
+
+console.log("Состояние: " + library.findBookBy("name", "Машина времени").state); //Состояние: 100
+
+console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+library.giveBookByName("Машина времени");
+console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
+let book = new PrintEditionItem("Машина времени", 1919, 138);
+book.state = 50;
+console.log("Книга: " + book.name + ", состояние: " + book.state); //Книга: Машина времени, состояние: 50
+book.fix();
+console.log("Книга: " + book.name + ", состояние: " + book.state); //Книга: Машина времени, состояние: 75
+book.author = "Герберт Уэллс";
+book.type = "detective";
+console.log(book); // {name: 'Машина времени', releaseDate: 1919, pagesCount: 138, _state: 75, type: 'detective', …}
+library.addBook(book);
+console.log("Количество книг после возврата: " + library.books.length); //Количество книг после выдачи: 4
+console.log("Книга в библиотеке: " + library.findBookBy("releaseDate", 1919).name); //Книга в библиотеке: Машина времени
