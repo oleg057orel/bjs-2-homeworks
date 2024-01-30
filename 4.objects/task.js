9d49adec77b571dc6a3b61d8daae67773869eb73
Lazarov_O_M_QAMID_61 <oleg057orel@yandex.ru>
2024-01-30 13:28:44 +0300

    Задание: Объекты

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,30 +1,32 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-    this.marks = [];
-}
+  this.name = name;
+  this.age = age;
+  this.gender = gender;
+  this.marks = [];
 
-Student.prototype.setSubject = function (subjectName) {
+  this.setSubject = function(subjectName) {
     this.subject = subjectName;
-}
+  };
 
-Student.prototype.addMarks = function (...marksToAdd) {
-  if (this.marks) {
+  this.addMarks = function(...marksToAdd) {
+    if (!this.marks) {
+      console.log("Error: Student has been expelled.");
+      return;
+    }
     this.marks.push(...marksToAdd);
-  }
-}
+  };
 
-Student.prototype.getAverage = function () {
-  if (!this.marks || this.marks == []) {
-    return 0;
-  } else {
-    return this.marks.reduce((acc, mark, index, arr) => acc + mark / arr.length, 0);
-  }
-}
+  this.getAverage = function() {
+    if (!this.marks || !this.marks.length) {
+      return 0;
+    }
+    const sum = this.marks.reduce((acc, curr) => acc + curr);
+    return sum / this.marks.length;
+  };
 
-Student.prototype.exclude = function (reason) {
-  delete this.subject;
-  delete this.marks;
-  this.excluded = reason;
+  this.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  };
 }
